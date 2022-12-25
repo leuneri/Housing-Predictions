dac8bb01495bd0c31e7839662c7efc16125c6c25
Eric Leung <ericleung147258369@hotmail.com>
2022-12-25 03:52:23 -0800

    able to create dataframes from webscrape data

--- housing_prediction.py
+++ housing_prediction.py
@@ -1,40 +1,53 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import time
+import os
+from tabulate import tabulate
 
 def houseSearch():
-    for i in range(100):
-        try:
-            html_text = requests.get('https://www.rew.ca/properties/areas/vancouver-bc/sort/latest/desc/page/{}?query=Vancouver%2C+BC'.format(i))
-            soup = BeautifulSoup(html_text.text, 'lxml')
-            houses = soup.findAll('div', class_ = 'displaypanel-content')
-            for house in houses:
-                try:
-                    price = house.find('div', class_ = "displaypanel-price hidden-xs").text
-                    location = house.find('ul', class_ = "inlinelist displaypanel-info")
-                    neighborhood = location.findAll("li")[0].text
-                    city = location.findAll("li")[1].text
-                    information = house.find('div', class_ = 'displaypanel-section clearfix')
-                    bedrooms = information.findAll("li")[0].text
-                    bathrooms = information.findAll("li")[1].text
-                    squareFeet = information.findAll("li")[2].text
-                    print(price)
-                    print(neighborhood)
-                    print(city)
-                    print(bedrooms)
-                    print(bathrooms)
-                    print(squareFeet)
-                except:
-                    print("MISSING INFORMATION")
-        except:
-            break
+    prices = []
+    neighborhoods = []
+    cities = []
+    bedrooms = []
+    bathrooms = []
+    squareFeet = []
+    for i in range(50):
+        html_text = requests.get('https://www.rew.ca/properties/areas/greater-vancouver-bc/sort/latest/desc/page/{}?query=Greater+Vancouver%2C+BC'.format(i))
+        soup = BeautifulSoup(html_text.text, 'lxml')
+        houses = soup.findAll('div', class_ = 'displaypanel-content')
+        for house in houses:
+            try:
+                price = house.find('div', class_ = "displaypanel-price hidden-xs").text
+                location = house.find('ul', class_ = "inlinelist displaypanel-info")
+                neighborhood = location.findAll("li")[0].text
+                city = location.findAll("li")[1].text
+                information = house.find('div', class_ = 'displaypanel-section clearfix')
+                bedroom = information.findAll("li")[0].text
+                bathroom = information.findAll("li")[1].text
+                size = information.findAll("li")[2].text
+                prices.append(price)
+                neighborhoods.append(neighborhood)
+                cities.append(city)
+                bedrooms.append(bedroom)
+                bathrooms.append(bathroom)
+                squareFeet.append(size)
+            except:
+                continue
+
+    dict = {'Price': prices, 'Neighbourhood': neighborhoods, 'City': cities, 'Bedrooms': bedrooms, 'Bathrooms': bathrooms, "Size (sq feet)": squareFeet}
+    df = pd.DataFrame(dict)
+    print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+    df.to_csv(os.getcwd()+'\\outputs.csv')
+    
     
 
+
 if __name__ == "__main__":
-    while True:
-        houseSearch()
-        print("Waiting for next cycle")
-        time.sleep(2678400) # 31 days
+    # while True:
+    houseSearch()
+    print("Waiting for next cycle")
+        # time.sleep(2678400) # 31 days
 
 
 ## Price, Neighborhood, City, Bedrooms, Bathrooms, Square Feet
